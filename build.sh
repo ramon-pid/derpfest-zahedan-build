135a9956c4cbe8ebeb24540d818b31240c4228e4
ramon <ramon@example.com>
2026-07-04 17:58:42 +0330

    fix: clone local manifest in build script

--- build.sh
+++ build.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
-set -e
 
 ROM_BRANCH="15.2"
 DEVICE="zahedan"
 MANIFEST_URL="https://github.com/DerpFest-LOS/android_manifest.git"
+LOCAL_MANIFEST_URL="https://github.com/ramon-pid/derpfest-zahedan-build.git"
 
 rm -rf .repo/local_manifests
-rm -rf device/daria vendor/daria kernel/daria
-rm -rf device/mediatek hardware/mediatek
+rm -rf {device,vendor,kernel}/daria
+rm -rf {device,hardware}/mediatek
 
 repo init -u ${MANIFEST_URL} -b ${ROM_BRANCH} --git-lfs --no-clone-bundle
 
-mkdir -p .repo/local_manifests
-cp local_manifests/local_manifest.xml .repo/local_manifests/local_manifest.xml
+git clone ${LOCAL_MANIFEST_URL} .repo/local_manifests
+rm -f .repo/local_manifests/build.sh
+rm -rf .repo/local_manifests/.git
 
 /opt/crave/resync.sh
 
 cd build/soong
 wget -O 0001-soong-HACK-disable-soong_filesystem_creator.patch \
-https://raw.githubusercontent.com/sajjad85gh/build-custom-rom/main/0001-soong-HACK-disable-soong_filesystem_creator.patch
+  https://raw.githubusercontent.com/sajjad85gh/build-custom-rom/main/0001-soong-HACK-disable-soong_filesystem_creator.patch
 git am 0001-soong-HACK-disable-soong_filesystem_creator.patch || true
-cd ../..
+cd -
 
 export BUILD_USERNAME=ramon
 export BUILD_HOSTNAME=crave
