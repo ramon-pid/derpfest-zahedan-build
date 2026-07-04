c39223abbfb027d717a0ff28d2f26843f8475aae
ramon <ramon@example.com>
2026-07-05 00:13:52 +0330

    force clean workspace before DerpFest sync

--- build.sh
+++ build.sh
@@ -6,19 +6,22 @@ DEVICE="zahedan"
 MANIFEST_URL="https://github.com/DerpFest-LOS/android_manifest.git"
 LOCAL_MANIFEST_URL="https://github.com/ramon-pid/zahedan-local-manifests.git"
 
-rm -rf .repo
-rm -rf device/daria kernel/daria vendor/daria
-rm -rf device/mediatek hardware/mediatek
+echo "Cleaning old workspace files..."
+find . -mindepth 1 -maxdepth 1 -exec rm -rf {} +
 
+echo "Initializing DerpFest source..."
 repo init -u "${MANIFEST_URL}" -b "${ROM_BRANCH}" --git-lfs --no-clone-bundle
 
+echo "Cloning local manifests..."
 git clone "${LOCAL_MANIFEST_URL}" .repo/local_manifests
 
+echo "Syncing source..."
 /opt/crave/resync.sh
 
 export BUILD_USERNAME=ramon
 export BUILD_HOSTNAME=crave
 
+echo "Starting build..."
 . build/envsetup.sh
 lunch lineage_${DEVICE}-bp1a-user
 mka derp
