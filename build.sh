b1d8e3ca5717199d2baee8d231081203efd1ea74
ramon <ramon@example.com>
2026-07-04 17:42:17 +0330

    feat: add DerpFest zahedan manifest and build script

--- build.sh
+++ build.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
+set -e
 
 ROM_BRANCH="15.2"
 DEVICE="zahedan"
 MANIFEST_URL="https://github.com/DerpFest-LOS/android_manifest.git"
 
 rm -rf .repo/local_manifests
-rm -rf {device,vendor,kernel}/daria
-rm -rf {device,hardware}/mediatek
+rm -rf device/daria vendor/daria kernel/daria
+rm -rf device/mediatek hardware/mediatek
 
 repo init -u ${MANIFEST_URL} -b ${ROM_BRANCH} --git-lfs --no-clone-bundle
 
-mkdir -p .repo
-cp -r local_manifests .repo/local_manifests
-rm -rf .repo/local_manifests/.git
+mkdir -p .repo/local_manifests
+cp local_manifests/local_manifest.xml .repo/local_manifests/local_manifest.xml
 
 /opt/crave/resync.sh
 
 cd build/soong
 wget -O 0001-soong-HACK-disable-soong_filesystem_creator.patch \
-  https://raw.githubusercontent.com/sajjad85gh/build-custom-rom/main/0001-soong-HACK-disable-soong_filesystem_creator.patch
+https://raw.githubusercontent.com/sajjad85gh/build-custom-rom/main/0001-soong-HACK-disable-soong_filesystem_creator.patch
 git am 0001-soong-HACK-disable-soong_filesystem_creator.patch || true
 cd ../..
 
