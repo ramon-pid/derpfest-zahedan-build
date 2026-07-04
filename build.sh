3523f213a5a25a19c28a04ed11a3d3c419ea1e87
ramon <ramon@example.com>
2026-07-04 19:31:25 +0330

    clean DerpFest build script

--- build.sh
+++ build.sh
@@ -6,24 +6,15 @@ DEVICE="zahedan"
 MANIFEST_URL="https://github.com/DerpFest-LOS/android_manifest.git"
 LOCAL_MANIFEST_URL="https://github.com/ramon-pid/zahedan-local-manifests.git"
 
-rm -rf .repo/local_manifests
-rm -rf {device,vendor,kernel}/daria
-rm -rf {device,hardware}/mediatek
+rm -rf .repo
+rm -rf device/daria kernel/daria vendor/daria
+rm -rf device/mediatek hardware/mediatek
 
-repo init -u ${MANIFEST_URL} -b ${ROM_BRANCH} --git-lfs --no-clone-bundle
+repo init -u "${MANIFEST_URL}" -b "${ROM_BRANCH}" --git-lfs --no-clone-bundle
 
-git clone ${LOCAL_MANIFEST_URL} .repo/local_manifests
+git clone "${LOCAL_MANIFEST_URL}" .repo/local_manifests
 
 /opt/crave/resync.sh
-
-cd build/soong
-wget -O 0001-soong-HACK-disable-soong_filesystem_creator.patch \
-  https://raw.githubusercontent.com/sajjad85gh/build-custom-rom/main/0001-soong-HACK-disable-soong_filesystem_creator.patch
-
-git am 0001-soong-HACK-disable-soong_filesystem_creator.patch || {
-    git am --abort || true
-}
-cd -
 
 export BUILD_USERNAME=ramon
 export BUILD_HOSTNAME=crave
